fd6b1cf968e963fdc52624a2d92975f3ade41375
vishalini2005 <vishalinivijayarajan395@gmail.com>
2025-12-10 12:57:38 +0530

    Ex:10 Simple and deep learning neural network model

---
+++ SimpleAndDeepLearningNNModel.py
@@ -0,0 +1,45 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms
+
+transform = transforms.Compose([transforms.ToTensor()])
+train_dataset = datasets.MNIST(root='.', train=True, download=True, transform=transform)
+test_dataset = datasets.MNIST(root='.', train=False, download=True, transform=transform)
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=64, shuffle=False)
+
+class SimpleNN(nn.Module):
+    def __init__(self):
+        super(SimpleNN, self).__init__()
+        self.fc1 = nn.Linear(28 * 28, 128)
+        self.fc2 = nn.Linear(128, 10)
+        self.relu = nn.ReLU()
+    def forward(self, x):
+        x = x.view(-1, 28 * 28)
+        x = self.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+model = SimpleNN()
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters())
+
+for epoch in range(5):
+    for data, target in train_loader:
+        optimizer.zero_grad()
+        output = model(data)
+        loss = criterion(output, target)
+        loss.backward()
+        optimizer.step()
+
+correct = 0
+total = 0
+with torch.no_grad():
+    for data, target in test_loader:
+        output = model(data)
+        _, predicted = torch.max(output.data, 1)
+        total += target.size(0)
+        correct += (predicted == target).sum().item()
+
+print(f'Test accuracy: {correct / total}')
